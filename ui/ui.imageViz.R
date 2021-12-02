8fbf5244072d2851f0471e60d30ac14960f586f1
mariaadissa <80592742+mariaadissa@users.noreply.github.com>
2021-12-02 14:26:54 +0100

    Correction function

--- ui/ui.imageViz.R
+++ ui/ui.imageViz.R
@@ -6,7 +6,9 @@ tabPanel(title = "Visualisation",
                       sidebarPanel(
                         shinyFilesButton("img_btn", "Choose an image" ,
                                          title = "Please select an image:", multiple = FALSE),
-                        verbatimTextOutput("img_text")
+                        verbatimTextOutput("img_text"),
+                        tags$br(),
+                        fileInput("infile", "Choose a csv file", multiple = FALSE, accept = c("text/csv","text/comma-separated-values,text/plain",".csv"))
                       ),
                       mainPanel(
                         fluidRow(tableOutput("data.table")),
@@ -21,7 +23,13 @@ tabPanel(title = "Visualisation",
                       ),
                       mainPanel(
                         fluidRow(column(width = 6, verbatimTextOutput("param.summary"))),
-                        fluidRow(plotlyOutput("boxplot"))
+                        fluidRow(plotlyOutput("boxplot")),
+                        fluidRow(tags$h3("Correction")),
+                        fluidRow(
+                          column(width = 2, numericInput("age",label = "Age",min = 1,value = 1)),
+                          column(width = 2, numericInput("val",label = "Value",min = 0,value = 1))
+                                ),
+                        fluidRow(column(width = 3, actionButton("correctBtn",label = "Remove")))
                       )
                     ))
          )
